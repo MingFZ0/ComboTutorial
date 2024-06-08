ffea248ec06a2153a3265771f5c9cf0a22cdedba
MingFZ0 <mingfengz1678@gmail.com>
2024-06-08 12:19:05 -0400

    Refactored PlayerAttack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -34,38 +34,51 @@ public class PlayerAttack : MonoBehaviour
         for (int i = 3; i < movesetPriorityMap.Keys.Count; i++)
         {
             MovesetPriorityLevel movesetPriorityLevel = movesetPriorityMap[i];
+            InputActionReference movesetLevelInput = movesetPriorityLevel.LevelInput;
+
+            if (movesetLevelInput.action.WasPressedThisFrame())
+            {
+                foreach (Move move in movesetPriorityLevel.Moves)
+                {
+                    if (move.DirectionalInput.action.IsPressed()) 
+                    {
+                        IsAttacking = true;
+                        actionScript.Action(move.MoveName);
+                    }
+                }
+            }
         }
     }
 
-    private void StartAttack()
-    {
-        if (playerControlsInput.Player.Down.IsPressed() && playerControlsInput.Player.HeavyAttack.WasPressedThisFrame()) HeavyAttack2H();
-        else if (playerControlsInput.Player.HeavyAttack.WasPressedThisFrame()) HeavyAttack5H();
-        else if (playerControlsInput.Player.Down.IsPressed() && playerControlsInput.Player.LightAttack.WasPressedThisFrame()) LightAttack2L();
-        else if (playerControlsInput.Player.LightAttack.WasPressedThisFrame()) LightAttack5L();
-    }
+    //private void StartAttack()
+    //{
+    //    if (playerControlsInput.Player.Down.IsPressed() && playerControlsInput.Player.HeavyAttack.WasPressedThisFrame()) HeavyAttack2H();
+    //    else if (playerControlsInput.Player.HeavyAttack.WasPressedThisFrame()) HeavyAttack5H();
+    //    else if (playerControlsInput.Player.Down.IsPressed() && playerControlsInput.Player.LightAttack.WasPressedThisFrame()) LightAttack2L();
+    //    else if (playerControlsInput.Player.LightAttack.WasPressedThisFrame()) LightAttack5L();
+    //}
 
-    private void LightAttack5L()
-    {
-        //Debug.Log("5L");
-        if(actionScript.Action(Attacks.Player_5L.ToString())) IsAttacking = true;
-    }
+    //private void LightAttack5L()
+    //{
+    //    //Debug.Log("5L");
+    //    if(actionScript.Action(Attacks.Player_5L.ToString())) IsAttacking = true;
+    //}
 
-    private void LightAttack2L()
-    {
-        //Debug.Log("2L");
-        if(actionScript.Action(Attacks.Player_2L.ToString())) IsAttacking = true;
-    }
+    //private void LightAttack2L()
+    //{
+    //    //Debug.Log("2L");
+    //    if(actionScript.Action(Attacks.Player_2L.ToString())) IsAttacking = true;
+    //}
 
-    private void HeavyAttack5H()
-    {
-        //Debug.Log("5H");
-        if (actionScript.Action(Attacks.Player_5H.ToString())) IsAttacking = true;
-    }
+    //private void HeavyAttack5H()
+    //{
+    //    //Debug.Log("5H");
+    //    if (actionScript.Action(Attacks.Player_5H.ToString())) IsAttacking = true;
+    //}
 
-    private void HeavyAttack2H()
-    {
-        //Debug.Log("2H");
-        if (actionScript.Action(Attacks.Player_2H.ToString())) IsAttacking = true;
-    }
+    //private void HeavyAttack2H()
+    //{
+    //    //Debug.Log("2H");
+    //    if (actionScript.Action(Attacks.Player_2H.ToString())) IsAttacking = true;
+    //}
 }
--- Assets/Scripts/System/MovesetMap.cs
+++ Assets/Scripts/System/MovesetMap.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEditor.Animations;
 using UnityEngine.InputSystem;
 using System.Linq;
 
